1837e4d77e9433a97aadea8820ff97296977d98b
mshxda <mariadruzinina140@gmail.com>
2025-01-20 21:05:32 +0300

    Add files via upload

---
+++ skript.js
@@ -0,0 +1,18 @@
+//задача 1.
+let str = 'abcde';
+alert(str[0]);
+alert(str[1]);
+alert(str[4]);
+
+//задача 2.
+let a = parseInt(prompt("Введите первое число"));
+let b = parseInt(prompt("Введите второе число"));
+console.log(a>0 || b>0);
+
+//задача 3.
+let age = parseInt(prompt("Введите возраст"));
+if (age>=18) {
+console.log("Совершеннолетний");
+} else {
+    console.log("Несовершеннолетний");
+}
